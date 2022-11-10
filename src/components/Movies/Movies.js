5409fb569ce2e90f17bd830db0d4772d5d9c9b91
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-11-10 20:14:20 +0300

    fixed

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList';
-import Preloader from '../Movies/Preloader/Preloader';
 import './Movies.css';
 
 export default function Movies({ movies, isMovies }) {
     return (
         <section className="movies">
-            <Preloader/>
             <div className="movies__item">
                 {movies.map(item => <MoviesCardList key={item.id} movie={item} isMovies={isMovies} />)}
             </div>
--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import Preloader from '../Preloader/Preloader';
 import './MoviesCard.css';
 import '../Movies.css';
 
 export default function MoviesCard({ movies, isMovies }) {
     return (
         <section className="movies-card">
-            <Preloader/>
             <div className="movies__item">
             {movies.map(item => <MoviesCardList key={item.id} movie={item} isMovies={isMovies} />)}
             </div>
