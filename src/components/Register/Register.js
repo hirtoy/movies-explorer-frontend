d7e5598e6994fd681db834ce19b909414f5a60d0
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-11-17 00:14:50 +0300

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {useForm} from "react-hook-form";
-import {CurrentUserContext} from '../../contexts/CurrentUserContext';
-import {useHistory} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { useHistory } from "react-router-dom";
 import LoginHeader from '../Login/form/LoginHeader/LoginHeader';
 // import LoginForm from '../Login/form/LoginForm/LoginForm';
 // import LoginSubmit from '../Login/form/LoginSubmit/LoginSubmit';
@@ -10,17 +10,18 @@ import LoginFooter from '../Login/form/LoginFooter/LoginFooter';
 // import '../Login/form/LoginHeader/LoginHeader.css';
 import './Register.css';
 
-export default function Register({onRegister}) {
+export default function Register({ onRegister }) {
     const [message, setMessage] = useState('');
     const user = React.useContext(CurrentUserContext);
     const history = useHistory();
 
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm({ mode: "onChange" });
+    const { register, handleSubmit,
+        formState: { errors, isValid } } = useForm({ mode: "onChange" });
     const onSubmit = (data) => {
         onRegister(data).catch((err) => {
             console.log(err);
             if (err === 'Ошибка: 409') {
-                setMessage('Пользователь с таким email уже существует');
+                setMessage('Такой пользователь уже существует');
             }
         })
         setTimeout(() => setMessage(''), 3000);
@@ -34,46 +35,46 @@ export default function Register({onRegister}) {
 
     return (
         <div className="register">
-            <LoginHeader title={'Добро пожаловать!'}/>
+            <LoginHeader title={'Добро пожаловать!'} />
             <form className="register__form" method="post" name="registerUserForm" onSubmit={handleSubmit(onSubmit)}
-                  noValidate>
+                noValidate>
                 <div className="register_form-filed">
                     <label className="register_form-filed-input-title">Имя</label>
                     <input type="text"
-                           placeholder="Имя"
-                           className={`register_form-filed-input ${errors?.name && 'register_form-filed-input_failed-validation'}`}
-                           minLength="2" maxLength="30" id={`redact-name-input`}
-                           required
-                           {...register('name', {
-                               required: 'Поле обязательно к заполнению', 
-                               minLength: { value: 2, message: 'Минимум 2 символа' }
-                           })}/>
+                        placeholder="Имя"
+                        className={`register_form-filed-input ${errors?.name && 'register_form-filed-input_failed-validation'}`}
+                        minLength="2" maxLength="30" id={`redact-name-input`}
+                        required
+                        {...register('name', {
+                            required: 'Поле обязательно к заполнению',
+                            minLength: { value: 2, message: 'Минимум 2 символа' }
+                        })} />
                     {errors?.name && <span className="register_form-filed-input-error">{errors?.name?.message}</span>}
                 </div>
                 <div className="register_form-filed">
                     <label className="register_form-filed-input-title">E-mail</label>
                     <input type="email"
-                           placeholder="pochta@yandex.ru"
-                           className={`register_form-filed-input ${errors?.email && 'register_form-filed-input_failed-validation'}`}
-                           minLength="2" maxLength="30" id={`redact-email-input`}
-                           required
-                           {...register('email', {
-                               required: 'Поле обязательно к заполнению', 
-                               pattern: { value: /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/, message: 'Неправильный формат email' }
-                           })}/>
+                        placeholder="pochta@yandex.ru"
+                        className={`register_form-filed-input ${errors?.email && 'register_form-filed-input_failed-validation'}`}
+                        minLength="2" maxLength="30" id={`redact-email-input`}
+                        required
+                        {...register('email', {
+                            required: 'Поле обязательно к заполнению',
+                            pattern: { value: /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/, message: 'Неправильный формат email' }
+                        })} />
                     {errors?.email && <span className="register_form-filed-input-error">{errors?.email?.message}</span>}
                 </div>
                 <div className="register_form-filed">
                     <label className="register_form-filed-input-title">Пароль</label>
                     <input type="password"
-                           placeholder=""
-                           className={`register_form-filed-input ${errors?.password && 'register_form-filed-input_failed-validation'}`}
-                           minLength="2" maxLength="30" id={`redact-password-input`}
-                           required
-                           {...register('password', {
-                               required: 'Поле обязательно к заполнению', 
-                               minLength: {value: 6, message: 'Минимум 6 символов'}
-                           })}/>
+                        placeholder=""
+                        className={`register_form-filed-input ${errors?.password && 'register_form-filed-input_failed-validation'}`}
+                        minLength="2" maxLength="30" id={`redact-password-input`}
+                        required
+                        {...register('password', {
+                            required: 'Поле обязательно к заполнению',
+                            minLength: { value: 6, message: 'Минимум 6 символов' }
+                        })} />
                     {errors?.password &&
                         <span className="register_form-filed-input-error">{errors?.password?.message}</span>}
                 </div>
@@ -84,7 +85,7 @@ export default function Register({onRegister}) {
                     </button>
                 </div>
             </form>
-            <LoginFooter title={'Уже зарегистрированы?'} linkText={'Войти'} link={'/signin'}/>
+            <LoginFooter title={'Уже зарегистрированы?'} linkText={'Войти'} link={'/signin'} />
         </div>
     )
 }
