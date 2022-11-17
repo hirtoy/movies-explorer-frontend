ac966a863dc55f9083592bc3f0bcf0ea8547cf78
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-11-17 18:21:27 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import {Link} from "react-router-dom";
 import HeaderNavigation from '../HeaderNavigation/HeaderNavigation';
 import './Header.css';
 
-export default function Header({loggedIn, aboutPage, onClick}) {
+export default function Header({loggedIn, aboutPage, onMenuClick}) {
     return (
         <header className={`header ${aboutPage && 'header__backraund'}`}>
             <Link to="/" className="header__logo"></Link>
             {loggedIn && <HeaderNavigation />}
-            <Auth loggedIn={loggedIn} onClick={onClick}/>
+            <Auth loggedIn={loggedIn} onClick={onMenuClick}/>
         </header>
     )
 }
