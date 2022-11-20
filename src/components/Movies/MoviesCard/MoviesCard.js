3d2c4ee043a3bb0a6356e91f2f62e73241e55470
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-11-20 04:55:54 +0300

    fixed

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -2,12 +2,10 @@ import React from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import './MoviesCard.css';
 import '../Movies.css';
-import Spinner from '../../Spinner/Spinner';
 
 export default function MoviesCard({ movies, isMovies, like, isLoading }) {
     return (
         <section className="movies-card">
-            {isLoading ? <Spinner/> : ''}
             <div className="movies__item">
 
                 {movies.map(item =>
