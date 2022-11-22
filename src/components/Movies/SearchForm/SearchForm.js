741027335675d77e9d1f085f8b5369ce829dbf64
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-11-22 20:40:34 +0300

    Update SearchForm.js

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import useFormWithValidation from '../../../utils/validateAutch';
+// import { CurrentUserContext } from '../../../contexts/CurrentUserContext';
 import './SearchForm.css';
 
 export default function SearchForm({ searchMovies }) {
     const [searchRequest, setSearchRequest] = useState(localStorage.getItem('searchRequest') ?? '');
     const [shortFilms, setShortFilms] = useState(localStorage.getItem('shortFilms') ?? 0);
-    const { values, isValid, handleChange } = useFormWithValidation();
+    const { values, handleChange, isValid } = useFormWithValidation();
+    // const currentUser = useContext(CurrentUserContext);
+
     const [message, setMessage] = useState('');
 
     function handleSearch() {
@@ -29,6 +32,7 @@ export default function SearchForm({ searchMovies }) {
         e.preventDefault();
         isValid ? handleSearch(values.search) : setMessage('Нужно ввести ключевое слово.');
     };
+
     useEffect(() => {
         setMessage('')
     }, [isValid]);
@@ -40,21 +44,20 @@ export default function SearchForm({ searchMovies }) {
 
     return (
         <section className="searchform">
-            <form className="searchform__items" name="search" onSubmit={onSubmit}>
+            <form className="searchform__items" noValidate name="search" onSubmit={onSubmit}>
                 <label className="searchform__input-item">
                     <input
-                    
                         name="search"
                         type="text"
                         className="searchform__input"
                         placeholder="Фильм"
                         autoComplete="off"
+                        required
                         onChange={handleChangeRequest}
                         value={searchRequest || ''}>
                     </input>
-
-                    <button type="button" className="searchform__input-submit" onChange={handleChange} onClick={handleSearch} >Поиск</button>
                     <span className="searchform__input-error">{message}</span>
+                    <button type="submit" className="searchform__input-submit" onChange={handleChange} onClick={handleSearch} >Поиск</button>
 
                 </label>
 
